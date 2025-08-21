80119a4e6141a429c8724c71f20557bf972da65b
katienkim <katie.kim@megazone.com>
2025-08-20 22:47:24 -0700

    Changed to module

--- new/nvirginia-alpha/main.tf
+++ new/nvirginia-alpha/main.tf
@@ -1,23 +1,23 @@
-module "2-tier" {
-  source = "../modules/2-tier"
+module "two-tier" {
+  source = "../module/2-tier"
 
   # provider variables
-  region = var.deploy_region
+  deploy_region = var.deploy_region
 
   # db variables
-  identifier = var.db_name
+  db_name = var.db_name
 
   # server variables
   ami = var.ami
-  tags = var.instance_tags
-  key_name = var.app_key
+  instance_tags = var.instance_tags
+  app_key = var.app_key
 
   # vpc variables
-  Name_vpc = var.vpc_name
-  Name_igw = var.igw_name
-   = var.db_subnet_group_name
-   = var.app_sg_name
-  var.db_sg_name
-  var.priv_rtb_name
-  var.public_rtb_name
+  vpc_name = var.vpc_name
+  igw_name = var.igw_name
+  db_subnet_group_name = var.db_subnet_group_name
+  app_sg_name = var.app_sg_name
+  db_sg_name = var.db_sg_name
+  priv_rtb_name = var.priv_rtb_name
+  public_rtb_name = var.public_rtb_name
 }
